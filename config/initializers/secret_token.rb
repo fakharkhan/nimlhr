f6c87d291f296dd8a9018bbb3420962a76e3bece
techverx <fakharkhan@live.com>
2012-11-11 17:42:44 +0500

    Created Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nimlhr::Application.config.secret_token = 'ad5f1a0b3d34a805d56c204836595906c7e2f01d3b78d4ba632d84714483433641e3db909b7377498c1863f61c7567d05ca3c2987d66a56fa554da3ed0eaeb02'
